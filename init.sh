358a443767768fe292c98f7e90bb58dae2cd4097
Gabor Garami <hron@aion.hu>
2017-10-16 10:18:09 +0200

    Small fixes

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ set -e
 
 DATADIR=/var/lib/mysql
 
-if [ ! "$(ls -A $DATADIR)" ]; then
+#if [ ! "$(ls -A $DATADIR)" ]; then
+if [ ! -d "${DATADIR}/mysql" ]; then
     SOCKET=/var/run/mysqld/mysqld.sock
 
     mkdir -p $(dirname ${SOCKET})
@@ -38,14 +39,14 @@ if [ ! "$(ls -A $DATADIR)" ]; then
     set -e
     echo ""
 
-    if [ "$i" = 30 ]; then
+    if [ "$i" -ge 30 ]; then
         echo 'MySQL init process failed, server is not available.' >&2
         exit 1
     fi
 
     INIT_SQL=""
     INIT_SQL="${INIT_SQL} SET @@SESSION.SQL_LOG_BIN=0;"
-    INIT_SQL="${INIT_SQL} DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost');"
+    INIT_SQL="${INIT_SQL} DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost', '${MYSQL_ROOT_HOST}');"
     INIT_SQL="${INIT_SQL} SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}');"
     if [ ! -z "$MYSQL_ROOT_HOST" -a "$MYSQL_ROOT_HOST" != 'localhost' ]; then
         INIT_SQL="${INIT_SQL} CREATE USER 'root'@'${MYSQL_ROOT_HOST}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
@@ -62,13 +63,14 @@ if [ ! "$(ls -A $DATADIR)" ]; then
         exit 1
     fi
 
-    echo "[client]" >~/.my.cnf
-    echo "user=root" >>~/.my.cnf
-    echo "password=${MYSQL_ROOT_PASSWORD}" >>~/.my.cnf
+    echo "[client]" > "${HOME}/.my.cnf"
+    echo "user=root" >> "${HOME}/.my.cnf"
+    echo "password=${MYSQL_ROOT_PASSWORD}" >> "${HOME}/.my.cnf"
+    chmod 0400 "${HOME}/.my.cnf"
 
     export MYSQL_ROOT_PASSWORD=""
 
     echo "MySQL initialization complete."
 fi
 
-$*
+exec "$@"
